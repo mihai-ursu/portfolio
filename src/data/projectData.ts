fc6eae9eb7eab4943c8343d4279eed83a5fa12b0
Mihai <mu@heyday.dk>
2023-09-25 09:01:52 +0300

    First commit

---
+++ src/data/projectData.ts
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        title: "Project 1",
+        description: "This is a project",
+        image: "images/project1.jpg",
+        url: "projects/project1",
+    },
+    {
+        title: "Project 2",
+        description: "This is a project",
+        image: "images/project1.jpg",
+        url: "projects/project2",
+    },
+];
+
+export default projects;
